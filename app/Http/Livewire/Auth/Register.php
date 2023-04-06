d3f128e59a2d735a76c3bada9c8bf33d272758dc
Charlotte <ckdrew670@hotmail.com>
2023-04-06 11:18:58 +0100

    Add basic form

--- app/Http/Livewire/Auth/Register.php
+++ app/Http/Livewire/Auth/Register.php
@@ -3,9 +3,24 @@
 namespace App\Http\Livewire\Auth;
 
 use Livewire\Component;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class Register extends Component
 {
+    public $name = '';
+    public $email = '';
+    public $password = '';
+    public $passwordConfirm = '';
+
+    public function register() {
+        User::create([
+            'name' => $this->name,
+            'email' => $this->email,
+            'password' => Hash::make($this->password)
+        ]);
+    }
+
     public function render()
     {
         return view('livewire.auth.register');
--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -1,4 +1,21 @@
-<div>
-    Shitpants
-    {{-- A good traveler has no fixed plans and is not intent upon arriving. --}}
-</div>
+<form wire:submit.prevent="register">
+    <div>
+        <label for="name">Name</label>
+        <input wire:model="name" type="text" id="name" name="name"/>
+    </div>
+    <div>
+        <label for="email">Email</label>
+        <input wire:model="email" type="text" id="email" name="email"/>
+    </div>
+    <div>
+        <label for="password">Password</label>
+        <input wire:model="password" type="password" id="password" name="password"/>
+    </div>
+    <div>
+        <label for="passwordConfirm">Confirm Password</label>
+        <input wire:model="passwordConfirm" type="password" id="passwordConfirm" name="passwordConfirm"/>
+    </div>
+    <div>
+        <input type="submit" value="Register"/>
+    </div>
+</form>
